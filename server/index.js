f9860475abe90fb4c92ee880583858115eb27570
aniketpuro <aniketpurohit851@gmail.com>
2023-11-18 12:44:22 +0530

    spelling mistack in PORT index is resolve

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.listen(POrt, () => {
+app.listen(PORT, () => {
     consolw.log(`Server listening on ${PORT}`);
 });
