4d57d6e7f05c7ae19864efb19f9a54d759340e81
aniketpuro <aniketpurohit851@gmail.com>
2023-11-18 12:45:27 +0530

    console seplling changed

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.listen(PORT, () => {
-    consolw.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
