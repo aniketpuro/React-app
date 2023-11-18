1b77edc305c24b1acc43a9170967c1cd5471ad29
aniketpuro <aniketpurohit851@gmail.com>
2023-11-18 15:19:03 +0530

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+  React.useEffect(()=> {
+    fetch("/api")
+    .then((res) => res.json())
+    .then((data) => setData(data.message));
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +17,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
- res.send('Hello World!');
+app.get("/api", (req, res) => {
+ res.json({ message: "Hello World!"});
 });
 
 app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`);
-});
+ console.log(`Server listening at http://localhost:${port}`);
+}); 
